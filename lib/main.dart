62e8d191db78aca0ae85dc994227c07365d761a0
Karlo Verde <karlo.verde.1996@gmail.com>
2021-06-29 23:04:10 -0500

    feat: add text controller demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'text_scroller/screen.dart';
+
+void main() => runApp(
+      MyApp(),
+    );
 
 class MyApp extends StatelessWidget {
   @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter UI',
+        home: _Home(),
+      );
+}
+
+class _Home extends StatelessWidget {
+  const _Home({
+    Key? key,
+  }) : super(key: key);
+
+  static const _routes = {
+    'Text Scroller': const TextScrollerScreen(),
+  };
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter UI',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Text('Flutter UI'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI samples'),
+      ),
+      body: GridView.count(
+        crossAxisCount: MediaQuery.of(context).size.width ~/ 200,
+        childAspectRatio: 2,
+        padding: const EdgeInsets.all(30.0),
+        children: [
+          ..._routes.keys.map(
+            (routeName) => ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => _routes[routeName]!,
+                  ),
+                );
+              },
+              child: Text(
+                routeName,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/text_scroller/screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TextScrollerScreen extends StatelessWidget {
+  const TextScrollerScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Text Scroller'),
+        ),
+      );
+}
